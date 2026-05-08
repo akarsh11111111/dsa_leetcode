8a4cb65c4d4f853b5c35ec3eae0d80ebdc640eb4
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-08 10:41:55 +0530

    Update 12thday_leetcode.cpp

--- 12thday_leetcode.cpp
+++ 12thday_leetcode.cpp
@@ -11,3 +11,4 @@ public:
 	string intToRoman(int num) {
 		vector<pair<int, string>> values = {
 			{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+			{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
