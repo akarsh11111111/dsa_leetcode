923ef2353b8a400520e0dac71ac49c4411e8aac4
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-09 17:14:18 +0530

    Update 23rdday_leetcode.cpp

--- 23rdday_leetcode.cpp
+++ 23rdday_leetcode.cpp
@@ -13,3 +13,4 @@ struct ListNode {
 	ListNode() : val(0), next(nullptr) {}
 	explicit ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* nextNode) : val(x), next(nextNode) {}
+};
