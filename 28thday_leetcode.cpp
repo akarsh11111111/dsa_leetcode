ef6b641718feaf6817b738e826996cf35172afd3
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-10 08:25:11 +0530

    Update 28thday_leetcode.cpp

--- 28thday_leetcode.cpp
+++ 28thday_leetcode.cpp
@@ -5,3 +5,4 @@ using namespace std;
 // LeetCode #28: Find the Index of the First Occurrence in a String
 // Return index of first occurrence of needle in haystack.
 // Time Complexity: O(n * m)
+// Space Complexity: O(1)
