b9e2cd175179d02b5339dd8c0b91ea38273e26f4
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-10 08:35:41 +0530

    Update 30thday_leetcode.cpp

--- 30thday_leetcode.cpp
+++ 30thday_leetcode.cpp
@@ -6,3 +6,4 @@ using namespace std;
 
 // LeetCode #30: Substring with Concatenation of All Words
 // Find all indices where a substring consisting of all words appears.
+// Time Complexity: O(n * m)
