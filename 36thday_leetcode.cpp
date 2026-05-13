a3570ac08b9a4450998b414f089205c82db699dc
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-13 07:13:00 +0530

    Update 36thday_leetcode.cpp

--- 36thday_leetcode.cpp
+++ 36thday_leetcode.cpp
@@ -12,3 +12,4 @@ public:
 	bool isValidSudoku(vector<vector<char>>& board) {
 		vector<vector<bool>> rows(9, vector<bool>(9, false));
 		vector<vector<bool>> cols(9, vector<bool>(9, false));
+		vector<vector<bool>> boxes(9, vector<bool>(9, false));
