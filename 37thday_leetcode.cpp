4acd967d3db81926de9ebdd3c7962a73cef3337c
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-14 07:40:23 +0530

    Update 37thday_leetcode.cpp

--- 37thday_leetcode.cpp
+++ 37thday_leetcode.cpp
@@ -20,3 +20,36 @@ private:
 
 				for (char num = '1'; num <= '9'; ++num) {
 					if (isValid(board, i, j, num)) {
+						board[i][j] = num;
+
+						if (backtrack(board)) return true;
+
+						board[i][j] = '.';
+					}
+				}
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+		for (int i = 0; i < 9; ++i) {
+			if (board[row][i] == num) return false;
+			if (board[i][col] == num) return false;
+		}
+
+		int boxRow = (row / 3) * 3;
+		int boxCol = (col / 3) * 3;
+
+		for (int i = boxRow; i < boxRow + 3; ++i) {
+			for (int j = boxCol; j < boxCol + 3; ++j) {
+				if (board[i][j] == num) return false;
+			}
+		}
+
+		return true;
+	}
+};
