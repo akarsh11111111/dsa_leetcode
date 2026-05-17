ca67acc05626b0839f8762e812102eb7e321130e
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-17 17:54:39 +0530

    Update 40thday_leetcode.cpp

--- 40thday_leetcode.cpp
+++ 40thday_leetcode.cpp
@@ -19,3 +19,4 @@ public:
 private:
 	void backtrack(vector<int>& candidates, int target, int start, vector<int>& path, vector<vector<int>>& result) {
 		if (target == 0) {
+			result.push_back(path);
