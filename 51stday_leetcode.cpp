08471df1dbd1cf9cd6040042ab4996d924de55be
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-24 20:18:41 +0530

    Update 51stday_leetcode.cpp

--- 51stday_leetcode.cpp
+++ 51stday_leetcode.cpp
@@ -9,3 +9,4 @@ using namespace std;
 // Given an integer n, return all distinct solutions to the n-queens puzzle.
 // Each solution contains an n x n board configuration of the placement of the
 // queens, where 'Q' and '.' both indicate a queen and an empty space respectively.
+// Example (n = 4):
