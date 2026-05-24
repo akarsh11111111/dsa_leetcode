66843134430bf704487905077f4e3c5d490869d2
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-24 20:21:26 +0530

    Update 51stday_leetcode.cpp

--- 51stday_leetcode.cpp
+++ 51stday_leetcode.cpp
@@ -47,3 +47,4 @@ public:
         vector<bool> diag1(2 * n - 1, false); // row + col
         vector<bool> diag2(2 * n - 1, false); // row - col + (n-1)
 
+        backtrack(0, n, queens, cols, diag1, diag2, res);
