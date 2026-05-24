14a0e20d975d12bc07f70b6146e6ca571ead6b83
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-24 20:18:34 +0530

    Update 51stday_leetcode.cpp

--- 51stday_leetcode.cpp
+++ 51stday_leetcode.cpp
@@ -7,3 +7,4 @@ using namespace std;
 // LeetCode #51: N-Queens
 // Problem:
 // Given an integer n, return all distinct solutions to the n-queens puzzle.
+// Each solution contains an n x n board configuration of the placement of the
