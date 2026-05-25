124d0ac6a2c3a28bb3aedaacc9e4dfb2fcc12aeb
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-25 12:01:46 +0530

    Update 52ndday_leetcode.cpp

--- 52ndday_leetcode.cpp
+++ 52ndday_leetcode.cpp
@@ -4,3 +4,4 @@ using namespace std;
 // LeetCode #52: N-Queens II
 // Problem: Count the number of distinct solutions to the n-queens puzzle.
 // Approach: Backtracking similar to N-Queens, but increment a counter when
+// a full placement is found. Use boolean arrays for columns and diagonals.
