7e5b2e78fcbde0b073f9e05b0605021671f37673
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-25 12:00:26 +0530

    Update 25thday_leetcode.cpp

--- 25thday_leetcode.cpp
+++ 25thday_leetcode.cpp
@@ -6,13 +6,13 @@ using namespace std;
 // Reverse nodes of a linked list k at a time.
 // Time Complexity: O(n)
 // Space Complexity: O(1)
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() : val(0), next(nullptr) {}
-	explicit ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode* nextNode) : val(x), next(nextNode) {}
-};
+// struct ListNode {
+// 	int val;
+// 	ListNode* next;
+// 	ListNode() : val(0), next(nullptr) {}
+// 	explicit ListNode(int x) : val(x), next(nullptr) {}
+// 	ListNode(int x, ListNode* nextNode) : val(x), next(nextNode) {}
+// };
 
 class Solution {
 public:
