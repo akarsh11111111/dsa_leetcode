c20d8a3ca09708c421c63849da1db8fb3c3496e7
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-25 12:03:05 +0530

    Update 52ndday_leetcode.cpp

--- 52ndday_leetcode.cpp
+++ 52ndday_leetcode.cpp
@@ -29,3 +29,4 @@ private:
             if (cols[col] || diag1[d1] || diag2[d2]) continue;
             queens[row] = col;
             cols[col] = diag1[d1] = diag2[d2] = true;
+            backtrack(row+1, n, queens, cols, diag1, diag2, count);
