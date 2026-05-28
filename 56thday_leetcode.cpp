9b49d6d66fbe00d7334b8fc500ea21ce05d9c5e8
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-28 11:25:50 +0530

    LeetCode #100: Same Tree
    LeetCode #100: Same Tree

--- 56thday_leetcode.cpp
+++ 56thday_leetcode.cpp
@@ -1,1 +1,25 @@
 #include <vector>
+using namespace std;
+
+// LeetCode #56: Merge Intervals
+// Problem: Merge all overlapping intervals.
+// Approach: Sort by start and merge sequentially.
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        if (intervals.empty()) return res;
+        sort(intervals.begin(), intervals.end());
+        res.push_back(intervals[0]);
+        for (size_t i = 1; i < intervals.size(); ++i) {
+            auto &last = res.back();
+            if (intervals[i][0] <= last[1]) {
+                last[1] = max(last[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
