a710ef4daa27d929c6f4648ae5865a12feca071d
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-29 12:39:31 +0530

    Update 57thday_leetcode.cpp

--- 57thday_leetcode.cpp
+++ 57thday_leetcode.cpp
@@ -14,3 +14,4 @@ public:
         int i = 0, n = intervals.size();
         while (i < n && intervals[i][1] < newInterval[0]) res.push_back(intervals[i++]);
         while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
