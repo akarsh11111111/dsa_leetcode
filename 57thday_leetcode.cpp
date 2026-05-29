8f151a243a7a0884bfb31e63b02355dded0fc145
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-29 12:39:27 +0530

    Update 57thday_leetcode.cpp

--- 57thday_leetcode.cpp
+++ 57thday_leetcode.cpp
@@ -12,3 +12,4 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
         int i = 0, n = intervals.size();
+        while (i < n && intervals[i][1] < newInterval[0]) res.push_back(intervals[i++]);
