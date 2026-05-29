7a6a5ea8787a576fb863c653244c514669bf71b7
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-29 12:38:57 +0530

    Update 57thday_leetcode.cpp

--- 57thday_leetcode.cpp
+++ 57thday_leetcode.cpp
@@ -4,3 +4,4 @@ using namespace std;
 // LeetCode #57: Insert Interval
 // Problem: Insert a new interval into a set of non-overlapping intervals and
 // merge if necessary.
+// Approach: Walk through intervals, add those before new interval, merge overlaps,
