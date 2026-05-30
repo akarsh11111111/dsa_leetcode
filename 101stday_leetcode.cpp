3358099de949c6ec1a1351444e8d6b0106ffd1bd
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-05-30 23:08:44 +0530

    Update 101stday_leetcode.cpp

--- 101stday_leetcode.cpp
+++ 101stday_leetcode.cpp
@@ -14,3 +14,4 @@ public:
         if (!a && !b) return true;
         if (!a || !b) return false;
         if (a->val != b->val) return false;
+        return check(a->left,b->right) && check(a->right,b->left);
