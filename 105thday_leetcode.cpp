d8c2f24e076a8a55d9f35913bfe490e2e715f548
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-02 20:42:50 +0530

    Update 105thday_leetcode.cpp

--- 105thday_leetcode.cpp
+++ 105thday_leetcode.cpp
@@ -6,3 +6,4 @@ using namespace std;
 // LeetCode #105: Construct Binary Tree from Preorder and Inorder
 struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
 
+class Solution {
