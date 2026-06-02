c8f9e300e00aa80b50a95041b4932c52e97c79ca
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-02 20:43:11 +0530

    Update 105thday_leetcode.cpp

--- 105thday_leetcode.cpp
+++ 105thday_leetcode.cpp
@@ -12,3 +12,4 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         for (int i=0;i<inorder.size();++i) idx[inorder[i]] = i;
         return build(preorder, 0, preorder.size()-1, 0, inorder.size()-1);
+    }
