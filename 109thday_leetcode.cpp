c5ab696325b34971a7a2e3cfa354acdc01a12142
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-05 21:37:03 +0530

    Update 109thday_leetcode.cpp

--- 109thday_leetcode.cpp
+++ 109thday_leetcode.cpp
@@ -5,3 +5,4 @@ using namespace std;
 struct ListNode { int val; ListNode* next; ListNode(int x): val(x), next(nullptr){} };
 struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
 
+class Solution {
