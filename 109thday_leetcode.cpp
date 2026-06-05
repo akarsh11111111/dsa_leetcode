118b78966547b71c47283a0d4c6ec36863b2ce81
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-05 21:36:56 +0530

    Update 109thday_leetcode.cpp

--- 109thday_leetcode.cpp
+++ 109thday_leetcode.cpp
@@ -3,3 +3,4 @@ using namespace std;
 
 // LeetCode #109: Convert Sorted List to Binary Search Tree
 struct ListNode { int val; ListNode* next; ListNode(int x): val(x), next(nullptr){} };
+struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
