8416a10d66a43248c681bd8ff7e8e90ef8407dd8
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-06 20:39:01 +0530

    Update 110thday_leetcode.cpp

--- 110thday_leetcode.cpp
+++ 110thday_leetcode.cpp
@@ -3,3 +3,4 @@
 using namespace std;
 
 // LeetCode #110: Balanced Binary Tree
+struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
