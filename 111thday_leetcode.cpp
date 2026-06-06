d59ec2ee3ad2fa4f63a1d28b8be9fbac072f495b
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-06 20:52:57 +0530

    Update 111thday_leetcode.cpp

--- 111thday_leetcode.cpp
+++ 111thday_leetcode.cpp
@@ -5,3 +5,4 @@ using namespace std;
 // LeetCode #111: Minimum Depth of Binary Tree
 struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
 
+class Solution {
