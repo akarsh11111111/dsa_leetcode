63ea042b2af3e3df04ca279339f0bfffeffe7db6
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-06 20:39:35 +0530

    Update 110thday_leetcode.cpp

--- 110thday_leetcode.cpp
+++ 110thday_leetcode.cpp
@@ -13,3 +13,4 @@ public:
     int height(TreeNode* node){
         if (!node) return 0;
         int lh = height(node->left); if (lh==-1) return -1;
+        int rh = height(node->right); if (rh==-1) return -1;
