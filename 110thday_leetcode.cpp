100f358ee24088aff8ad8f32e57d556a0872fe22
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-06 20:39:38 +0530

    Update 110thday_leetcode.cpp

--- 110thday_leetcode.cpp
+++ 110thday_leetcode.cpp
@@ -14,3 +14,4 @@ public:
         if (!node) return 0;
         int lh = height(node->left); if (lh==-1) return -1;
         int rh = height(node->right); if (rh==-1) return -1;
+        if (abs(lh-rh)>1) return -1;
