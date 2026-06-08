12c42e31322180ff911e5abf4f08b4b102e33901
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-08 08:50:37 +0530

    Update 112ndday_leetcode.cpp

--- 112ndday_leetcode.cpp
+++ 112ndday_leetcode.cpp
@@ -9,3 +9,4 @@ public:
     bool hasPathSum(TreeNode* root, int sum) {
         if (!root) return false;
         if (!root->left && !root->right) return sum==root->val;
+        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
