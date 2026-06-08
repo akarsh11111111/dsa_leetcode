b006a6e25e66473f6f1494f26fe6cbcbbd35eef0
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-08 08:50:41 +0530

    Update 112ndday_leetcode.cpp

--- 112ndday_leetcode.cpp
+++ 112ndday_leetcode.cpp
@@ -10,3 +10,4 @@ public:
         if (!root) return false;
         if (!root->left && !root->right) return sum==root->val;
         return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+    }
