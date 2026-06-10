0db89900d2fb64502cc43b8379ac389899dfa0ff
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-10 18:16:59 +0530

    Update 113rdday_leetcode.cpp

--- 113rdday_leetcode.cpp
+++ 113rdday_leetcode.cpp
@@ -3,3 +3,4 @@
 using namespace std;
 
 // LeetCode #113: Path Sum II (collect paths)
+struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
