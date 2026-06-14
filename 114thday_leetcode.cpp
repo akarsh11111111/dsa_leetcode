57b2ff7a0f03aff2be4320afd4a82746f343fbe4
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-14 19:10:16 +0530

    Update 114thday_leetcode.cpp

--- 114thday_leetcode.cpp
+++ 114thday_leetcode.cpp
@@ -2,3 +2,4 @@
 using namespace std;
 
 // LeetCode #114: Flatten Binary Tree to Linked List
+struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
