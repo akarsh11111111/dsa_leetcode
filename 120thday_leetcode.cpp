1f504be97a8de2e564f681908d7d6e144cdae90d
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-21 12:04:04 +0530

    Update 120thday_leetcode.cpp

--- 120thday_leetcode.cpp
+++ 120thday_leetcode.cpp
@@ -3,3 +3,16 @@
 #include <iostream>
 using namespace std;
 
+// LeetCode #120: Triangle (minimum total)
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& tri) {
+        if (tri.empty()) return 0;
+        vector<int> dp = tri.back();
+        for (int i=(int)tri.size()-2;i>=0;i--){
+            for (int j=0;j<tri[i].size();j++) dp[j] = tri[i][j] + min(dp[j], dp[j+1]);
+        }
+        return dp[0];
+    }
+};
+
