6d83eff9dea0a9112fbc05f43f3218f561061a7e
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-23 13:54:06 +0530

    Update 123rdday_leetcode.cpp

--- 123rdday_leetcode.cpp
+++ 123rdday_leetcode.cpp
@@ -13,3 +13,4 @@ public:
         for (int i=1;i<n;i++){ minp = min(minp, prices[i]); left[i] = max(left[i-1], prices[i]-minp); }
         int maxp = prices[n-1];
         for (int i=n-2;i>=0;i--){ maxp = max(maxp, prices[i]); right[i] = max(right[i+1], maxp-prices[i]); }
+        int res=0; for (int i=0;i<n;i++) res = max(res, left[i]+right[i]); return res;
