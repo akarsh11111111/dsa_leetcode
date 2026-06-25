99cf4c79b56e6f38974dcd48579f610180439ffe
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-25 10:21:01 +0530

    Update 124thday_leetcode.cpp

--- 124thday_leetcode.cpp
+++ 124thday_leetcode.cpp
@@ -4,3 +4,4 @@
 using namespace std;
 
 // LeetCode #124: Binary Tree Maximum Path Sum
+struct TreeNode { int val; TreeNode* left; TreeNode* right; TreeNode(int x): val(x), left(nullptr), right(nullptr){} };
