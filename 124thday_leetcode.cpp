3e9b51c3d83702948cc3b566732ef1b231bcff25
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-25 10:21:39 +0530

    Update 124thday_leetcode.cpp

--- 124thday_leetcode.cpp
+++ 124thday_leetcode.cpp
@@ -15,3 +15,4 @@ public:
         int l = max(0, helper(node->left));
         int r = max(0, helper(node->right));
         best = max(best, node->val + l + r);
+        return node->val + max(l,r);
