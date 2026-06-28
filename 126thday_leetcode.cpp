71da35a739342267d1e5e72265fe0bebeceaad3b
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-06-28 10:31:35 +0530

    Update 126thday_leetcode.cpp

--- 126thday_leetcode.cpp
+++ 126thday_leetcode.cpp
@@ -10,3 +10,4 @@ using namespace std;
 // Note: uses BFS + backtracking (may be heavy but acceptable as template)
 class Solution {
 public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
