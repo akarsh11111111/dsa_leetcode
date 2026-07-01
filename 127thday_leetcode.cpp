a12655c20d5c719c02d1850086210eb5ab5901e0
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-01 10:11:19 +0530

    Update 127thday_leetcode.cpp

--- 127thday_leetcode.cpp
+++ 127thday_leetcode.cpp
@@ -12,3 +12,4 @@ public:
         unordered_set<string> dict(wordList.begin(), wordList.end());
         if (!dict.count(endWord)) return 0;
         queue<string> q; q.push(beginWord); int steps=1;
+        while (!q.empty()){
