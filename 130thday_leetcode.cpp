154ae61a36dcbe8621a3e511085ba5d070f46ff6
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-04 18:53:13 +0530

    Update 130thday_leetcode.cpp

--- 130thday_leetcode.cpp
+++ 130thday_leetcode.cpp
@@ -22,3 +22,4 @@ public:
         while (!q.empty()){
             auto [x,y]=q.front(); q.pop();
             for (auto &d: dirs){ int nx=x+d[0], ny=y+d[1];
+                if (nx>=0 && nx<m && ny>=0 && ny<n && board[nx][ny]=='O'){ board[nx][ny]='T'; q.push({nx,ny}); }
