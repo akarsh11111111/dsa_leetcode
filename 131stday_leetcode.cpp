39ae6cbb0f2f56dae8c94789ebdd689cffd421f6
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-05 18:06:49 +0530

    Update 131stday_leetcode.cpp

--- 131stday_leetcode.cpp
+++ 131stday_leetcode.cpp
@@ -13,3 +13,4 @@ public:
         vector<vector<bool>> pal(n, vector<bool>(n,false));
         for (int i=n-1;i>=0;--i){
             for (int j=i;j<n;++j){
+                if (s[i]==s[j] && (j-i<2 || pal[i+1][j-1])) pal[i][j]=true;
