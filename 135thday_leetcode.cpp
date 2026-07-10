a2c0fab784b44604a43c0354603c39721f451a22
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-10 10:18:49 +0530

    Update 135thday_leetcode.cpp

--- 135thday_leetcode.cpp
+++ 135thday_leetcode.cpp
@@ -9,3 +9,4 @@ public:
         int n = ratings.size();
         if (n==0) return 0;
         vector<int> left(n,1), right(n,1);
+        for (int i=1;i<n;i++) if (ratings[i]>ratings[i-1]) left[i]=left[i-1]+1;
