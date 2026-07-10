8f3cfa0971199c1a73efc23a5199d8c61d2bb8d8
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-10 10:19:02 +0530

    Update 135thday_leetcode.cpp

--- 135thday_leetcode.cpp
+++ 135thday_leetcode.cpp
@@ -13,3 +13,4 @@ public:
         for (int i=n-2;i>=0;i--) if (ratings[i]>ratings[i+1]) right[i]=right[i+1]+1;
         int ans=0; for (int i=0;i<n;i++) ans += max(left[i], right[i]);
         return ans;
+    }
