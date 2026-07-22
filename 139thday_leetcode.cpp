8b7729aec2451f9d8ed5cdc675bfc50286fc8c74
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-22 12:08:02 +0530

    Update 139thday_leetcode.cpp

--- 139thday_leetcode.cpp
+++ 139thday_leetcode.cpp
@@ -10,3 +10,4 @@ public:
     bool wordBreak(const string& s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         int n = s.size(); vector<bool> dp(n+1,false); dp[0]=true;
+        for (int i=1;i<=n;i++){
