c9f7cfacc9d06f2fdb9f90e7ae9ccc6a7b679034
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-23 17:32:03 +0530

    Update 140thday_leetcode.cpp

--- 140thday_leetcode.cpp
+++ 140thday_leetcode.cpp
@@ -9,3 +9,4 @@ using namespace std;
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
