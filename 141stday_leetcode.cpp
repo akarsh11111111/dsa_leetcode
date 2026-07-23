f5970dc1ce6d0aa6231df9fae23f292d7489a75b
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-23 22:13:27 +0530

    Update 141stday_leetcode.cpp

--- 141stday_leetcode.cpp
+++ 141stday_leetcode.cpp
@@ -7,3 +7,4 @@ struct ListNode { int val; ListNode* next; ListNode(int x): val(x), next(nullptr
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
+        ListNode *slow=head, *fast=head;
