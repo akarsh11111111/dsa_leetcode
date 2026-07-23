6c2845e912658f5f9fbf0daf8162afce689c072e
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-07-23 22:13:37 +0530

    Update 141stday_leetcode.cpp

--- 141stday_leetcode.cpp
+++ 141stday_leetcode.cpp
@@ -10,3 +10,4 @@ public:
         ListNode *slow=head, *fast=head;
         while (fast && fast->next){ slow=slow->next; fast=fast->next->next; if (slow==fast) return true; }
         return false;
+    }
