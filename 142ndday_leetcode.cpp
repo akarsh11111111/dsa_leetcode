88538840c695cd90705d83158698743858eb677b
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-08-01 23:20:46 +0530

    Update 142ndday_leetcode.cpp

--- 142ndday_leetcode.cpp
+++ 142ndday_leetcode.cpp
@@ -10,3 +10,4 @@ public:
         ListNode *slow=head, *fast=head;
         while (fast && fast->next){ slow=slow->next; fast=fast->next->next; if (slow==fast) break; }
         if (!fast || !fast->next) return nullptr;
+        slow = head;
