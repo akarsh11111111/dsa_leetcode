9c8337e763aeb069bbd349b9a08380693825a827
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-08-01 23:20:49 +0530

    Update 142ndday_leetcode.cpp

--- 142ndday_leetcode.cpp
+++ 142ndday_leetcode.cpp
@@ -11,3 +11,4 @@ public:
         while (fast && fast->next){ slow=slow->next; fast=fast->next->next; if (slow==fast) break; }
         if (!fast || !fast->next) return nullptr;
         slow = head;
+        while (slow!=fast){ slow=slow->next; fast=fast->next; }
