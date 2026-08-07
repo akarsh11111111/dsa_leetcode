8e90728a3c307ba7211d95eac00afbf50ba7368f
akarsh vidyarthi <168165668+akarsh11111111@users.noreply.github.com>
2026-08-07 20:53:53 +0530

    Update 144thday_leetcode.cpp

--- 144thday_leetcode.cpp
+++ 144thday_leetcode.cpp
@@ -10,3 +10,14 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res; if (!root) return res;
+        stack<TreeNode*> st; st.push(root);
+        while (!st.empty()){
+            TreeNode* node = st.top(); st.pop(); res.push_back(node->val);
+            if (node->right) st.push(node->right);
+            if (node->left) st.push(node->left);
+        }
+        return res;
+    }
+};
+
+int main(){ return 0; }
